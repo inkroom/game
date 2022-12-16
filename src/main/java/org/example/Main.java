8374ecada58c16ab7c0263e570e96bcb42f07214
inkbox <enpassPixiv@protonmail.com>
2022-12-16 14:23:03 +0800

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * @author inkbox
+ * @since ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup work = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(boss, work);
+            bootstrap.channel(NioServerSocketChannel.class);
+            bootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+//                    ch.pipeline().addLast("logging",new LoggingHandler("DEBUG"));//设置log监听器，并且日志级别为debug，方便观察运行流程
+                    ChannelPipeline pipeline = ch.pipeline();
+                    pipeline.addLast(new HttpServerCodec());
+                    pipeline.addLast(new HttpObjectAggregator(64 * 1024));
+                    pipeline.addLast(new ChunkedWriteHandler());
+                    pipeline.addLast(new WebSocketServerProtocolHandler("/sw"));
+                    pipeline.addLast(new TextWebSocketHandler());
+                }
+            });
+            Channel channel = bootstrap.bind(38293).sync().channel();
+            System.out.println("webSocket服务器启动成功：" + channel);
+            channel.closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.out.println("运行出错：" + e);
+        } finally {
+            boss.shutdownGracefully();
+            work.shutdownGracefully();
+        }
+    }
+}
